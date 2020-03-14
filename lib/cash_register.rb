e5e7814c7a6320186c500590e4b781b8ea94b7f9
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-14 02:27:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,7 +37,6 @@ class CashRegister
     @total=@total - @disc_amount
     if @disc_amount > 0
       puts "After the discount, the total comes to $#{@total}."
-      return @total
     else
       puts "There is no discount to apply."
       return @total
