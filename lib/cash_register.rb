3117f396dec8179e9863229b27e913da089fece1
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-14 03:31:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -38,12 +38,13 @@ class CashRegister
       @disc_amount=( @discount * @price ) / 100
       @total=@total - @disc_amount
       
-    if @disc_amount > 0
-      return "After the discount, the total comes to $#{@total}."
-    else
-      return "There is no discount to apply."
-    end 
-  end
+      if @disc_amount > 0
+        return "After the discount, the total comes to $#{@total}."
+      else
+        return "There is no discount to apply."
+      end 
+    end
+  end 
 
 
 
