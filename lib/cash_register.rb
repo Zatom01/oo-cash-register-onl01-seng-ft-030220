d5ff7c82539249e98b42a18fca0b8afc49c59775
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-14 00:35:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+class CashRegister
+  
+  attr_accessor :total, :discount, :items
 
+
+  def initialize(num=0)
+    @total=num
+    @discount=20
+    @items=[]
+  end 
+
+  def add_item(title,quantity=1,price)
+    return @total= @total + (quantity * price)
+    # @similar_arr=[] 
+    # while quantity!=0 do
+    #   @similar_arr << title
+    #   quantity -= 1 
+    # end
+
+    # @similar_arr.each do |e|
+    #   @items << e 
+    # end 
+
+    
+  end 
+
+  def apply_discount
+    @total=@total- (0.2 * @total).to_i
+    puts "After the discount, the total comes to $#{@total}."
+    return @total
+  end 
+
+  def total
+    @total
+  end 
+
+  def items 
+    @items
+  end 
+  
+  
+end
